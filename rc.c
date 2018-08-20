cc60593ae2311d2c7039178c2e628bb60cc3cbb2
Yota Toyama <raviqqe@gmail.com>
2018-08-20 23:01:17 +0000

    Use atomic operations

--- rc.c
+++ rc.c
@@ -4,12 +4,14 @@ static void *rc_alloc_ptr(rc_t rc) {
   return (void *)((size_t)(rc.ptr) - sizeof(size_t));
 }
 
-static size_t *rc_count(rc_t rc) { return (size_t *)(rc_alloc_ptr(rc)); }
+static atomic_size_t *rc_count(rc_t rc) {
+  return (atomic_size_t *)(rc_alloc_ptr(rc));
+}
 
 rc_t rc_malloc(size_t size) {
   rc_t rc = (rc_t){malloc(sizeof(size_t) + size)};
 
-  (*rc_count(rc))++;
+  atomic_store(rc_count(rc), 1);
 
   return rc;
 }
@@ -21,11 +23,11 @@ void rc_move(rc_t src, rc_t *dst) {
 }
 
 void rc_free(rc_t *rc) {
-  size_t *count = rc_count(*rc);
+  atomic_size_t *count = rc_count(*rc);
 
-  (*count)--;
+  atomic_fetch_sub(count, 1);
 
-  if (*count == 0) {
+  if (atomic_load(count) == 0) {
     free(rc_alloc_ptr(*rc));
   } else {
     rc->ptr = NULL;
--- rc.h
+++ rc.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdatomic.h>
 #include <stdlib.h>
 
 typedef struct { void *ptr; } rc_t;
