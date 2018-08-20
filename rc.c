c08ef87ca45748a60062f6a83b90840c8f6be155
Yota Toyama <raviqqe@gmail.com>
2018-08-20 23:25:48 +0000

    Define utility functions

--- rc.c
+++ rc.c
@@ -6,10 +6,15 @@ static void *rc_alloc_ptr(rc_t rc) { return rc.ptr - count_size; }
 
 static atomic_size_t *rc_count(rc_t rc) { return rc_alloc_ptr(rc); }
 
+static void rc_increment_count(rc_t rc) { atomic_fetch_add(rc_count(rc), 1); }
+static void rc_decrement_count(rc_t rc) { atomic_fetch_sub(rc_count(rc), 1); }
+static void rc_initialize_count(rc_t rc) { atomic_store(rc_count(rc), 1); }
+static size_t rc_load_count(rc_t rc) { return atomic_load(rc_count(rc)); }
+
 rc_t rc_malloc(size_t size) {
   rc_t rc = (rc_t){malloc(count_size + size) + count_size};
 
-  atomic_store(rc_count(rc), 1);
+  rc_initialize_count(rc);
 
   return rc;
 }
@@ -21,11 +26,9 @@ void rc_move(rc_t src, rc_t *dst) {
 }
 
 void rc_free(rc_t *rc) {
-  atomic_size_t *count = rc_count(*rc);
+  rc_decrement_count(*rc);
 
-  atomic_fetch_sub(count, 1);
-
-  if (atomic_load(count) == 0) {
+  if (rc_load_count(*rc) == 0) {
     free(rc_alloc_ptr(*rc));
   } else {
     rc->ptr = NULL;
