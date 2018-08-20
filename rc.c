7c630205a1b1030d50aacaddcf4db8e1ef416573
Yota Toyama <raviqqe@gmail.com>
2018-08-20 23:05:56 +0000

    Remove some explicit conversions

--- rc.c
+++ rc.c
@@ -1,12 +1,8 @@
 #include "rc.h"
 
-static void *rc_alloc_ptr(rc_t rc) {
-  return (void *)((size_t)(rc.ptr) - sizeof(size_t));
-}
+static void *rc_alloc_ptr(rc_t rc) { return rc.ptr - sizeof(size_t); }
 
-static atomic_size_t *rc_count(rc_t rc) {
-  return (atomic_size_t *)(rc_alloc_ptr(rc));
-}
+static atomic_size_t *rc_count(rc_t rc) { return rc_alloc_ptr(rc); }
 
 rc_t rc_malloc(size_t size) {
   rc_t rc = (rc_t){malloc(sizeof(size_t) + size)};
